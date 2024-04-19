e5df8935cbead94ee7babf33fe4b0f4e3eca329e
Murat Can <141832088+codcreater1@users.noreply.github.com>
2024-04-19 12:13:15 +0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -2,11 +2,11 @@ import random
 import time
 
 
-print("\nWelcome to Hangman game \n")
+print("Welcome to Hangman game ")
 name = input("Enter your name: ")
 print("Hello " + name + "! Best of Luck!")
 time.sleep(2)
-print("The game is about to start!\n Let's play Hangman!")
+print("The game is about to start! : Let's play Hangman!")
 time.sleep(3)
 
 
